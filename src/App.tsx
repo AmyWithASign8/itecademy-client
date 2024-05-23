f4c7cc3edd59d7ee551e70444d8275ce73d654aa
danil <progermegacount@gmail.com>
2024-05-23 11:44:01 +0500

    🚧 wip: injectable auth routes if user are not auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { Route, Routes } from 'react-router-dom'
 import { GeneralPage, LoginPage, RegistrationPage } from './pages'
 import { MainLayout } from './layouts'
+import { observer } from 'mobx-react-lite'
+import UserStore from './common/store/user'
 
 function App() {
+  const { getIsAuth } = UserStore
+
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<GeneralPage />} />
-        <Route path="registration" element={<RegistrationPage />} />
-        <Route path="login" element={<LoginPage />} />
+        {!getIsAuth && (
+          <>
+            <Route path="registration" element={<RegistrationPage />} />
+            <Route path="login" element={<LoginPage />} />
+          </>
+        )}
       </Route>
     </Routes>
   )
 }
 
-export default App
+export default observer(App)
