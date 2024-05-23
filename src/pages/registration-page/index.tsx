ecb2b3a055c01c8ebf2ed9a78b408ba929c92fe5
danil <progermegacount@gmail.com>
2024-05-23 17:38:54 +0500

    🚧 wip: registration query

--- src/pages/registration-page/index.tsx
+++ src/pages/registration-page/index.tsx
@@ -1,16 +1,41 @@
-import { Button, Flex, PasswordInput, TextInput } from '@mantine/core'
+import { Alert, Button, Flex, PasswordInput, TextInput } from '@mantine/core'
 import { SubmitHandler, useForm } from 'react-hook-form'
-import { AuthDataType } from '../../common/types/auth.interface'
-import { IconAt, IconLock } from '@tabler/icons-react'
+import { AuthDataType, ErrorResponse } from '../../common/types/auth.interface'
+import { IconAlertCircle, IconAt, IconLock } from '@tabler/icons-react'
+import { registration } from '../../common/services/user/user'
+import { useState } from 'react'
+import { observer } from 'mobx-react-lite'
+import UserStore from '../../common/store/user'
+import { User } from '../../common/store/user/user.interface'
 
-export const RegistrationPage = () => {
+export const RegistrationPage = observer(() => {
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm<AuthDataType>()
 
-  const onSubmit: SubmitHandler<AuthDataType> = (data) => console.log(data)
+  const [loading, setIsLoading] = useState(false)
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  const onSubmit: SubmitHandler<AuthDataType> = async (data) => {
+    setIsLoading(true)
+    try {
+      const response = await registration(data)
+
+      if ((response as ErrorResponse).message) {
+        setErrorMessage((response as ErrorResponse).message)
+      }
+
+      UserStore.setUser = response as User
+      UserStore.setIsAuth = true
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -47,11 +72,20 @@ export const RegistrationPage = () => {
             placeholder="Введите ваш пароль"
             icon={<IconLock size="1rem" />}
           />
-          <Button fullWidth color={'gray'} type="submit">
+          <Button fullWidth color={'gray'} type="submit" loading={loading}>
             {'Зарегистрироваться'}
           </Button>
+          {errorMessage && (
+            <Alert
+              icon={<IconAlertCircle size="1rem" />}
+              title="Ошибка!"
+              color="red"
+            >
+              {errorMessage}
+            </Alert>
+          )}
         </Flex>
       </Flex>
     </form>
   )
-}
+})
