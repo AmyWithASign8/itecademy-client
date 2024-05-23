5fa52e22b12320c83e0b825db66adb3d68596551
danil <progermegacount@gmail.com>
2024-05-23 11:44:18 +0500

    ✨ feat: `QueryClientProvider`

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,7 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         withGlobalStyles
         withNormalizeCSS
       >
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>
