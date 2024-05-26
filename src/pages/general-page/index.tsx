0f6e3ebe21939348ae7f6680f2c83d64e4e6a49c
DanilFrontend <danilstorage@gmail.com>
2024-05-27 03:29:43 +0500

    💄 ui: added alert for unauthorize users

--- src/pages/general-page/index.tsx
+++ src/pages/general-page/index.tsx
@@ -1,10 +1,14 @@
-import { Center, SimpleGrid, Text } from '@mantine/core'
+import { Alert, Button, Center, Flex, SimpleGrid, Text } from '@mantine/core'
 import { Card } from '../../components/card/card'
 import CourseStore from '../../common/store/course'
+import UserStore from '../../common/store/user'
 import { observer } from 'mobx-react-lite'
+import { IconAlertCircle } from '@tabler/icons-react'
+import { Link } from 'react-router-dom'
 
 export const GeneralPage = observer(() => {
   const { getCourses } = CourseStore
+  const { getIsAuth } = UserStore
 
   return (
     <>
@@ -15,6 +19,36 @@ export const GeneralPage = observer(() => {
           деньги за программу
         </Text>
       </Center>
+      {!getIsAuth && (
+        <Center>
+          <Alert
+            mt={100}
+            icon={<IconAlertCircle size="1rem" />}
+            title="Внимание!"
+            color="gray"
+            w={'50%'}
+          >
+            <Text>
+              Авторизованный пользователь получает повышенную безопасность,
+              доступ к персонализированному контенту и функциям. Это улучшает
+              удобство и качество взаимодействия с платформой.
+            </Text>
+            <Flex justify={'space-around'} mt={30}>
+              <Button w={'35%'} component={Link} to={'/login'} color="gray">
+                {'Войти'}
+              </Button>
+              <Button
+                w={'35%'}
+                component={Link}
+                to={'/registration'}
+                color="gray"
+              >
+                {'Зарегистрироваться'}
+              </Button>
+            </Flex>
+          </Alert>
+        </Center>
+      )}
       <SimpleGrid cols={4} mt={100} p={'md'}>
         {getCourses.map((course) => (
           <Card data={course} />
